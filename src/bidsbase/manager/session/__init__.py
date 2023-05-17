0410165b4eb70aa7b5e45376eb425ca1587e31b8
GalKepler <galkepler@gmail.com>
2023-05-17 13:50:16 +0300

    removed make fieldmap from dwi

--- src/bidsbase/manager/session/__init__.py
+++ src/bidsbase/manager/session/__init__.py
@@ -1,4 +1,3 @@
 from bidsbase.manager.session.common_fixes import fix_multiple_dwi_runs
-from bidsbase.manager.session.common_fixes import generate_fieldmap_from_dwi
 
-COMMON_FIXES = [fix_multiple_dwi_runs, generate_fieldmap_from_dwi]
+COMMON_FIXES = [fix_multiple_dwi_runs]
